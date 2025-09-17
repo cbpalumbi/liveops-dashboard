0e677c68302d502e431c0527870afe010edad859
cbpalumbi <christabellapalumbi@gmail.com>
2025-09-16 20:53:04 -0400

    Add Run Simulation button

--- ml_liveops_dashboard/src/components/NotYetRunCampaign.jsx
+++ ml_liveops_dashboard/src/components/NotYetRunCampaign.jsx
@@ -1,12 +1,33 @@
-export default function SimulationPage({ campaign }) {
+import { useState, useEffect } from "react";
 
-    // should show the relevant static details of the campaign
-    // should show a button to run the campaign
+export default function SimulationPage({ campaign }) {
+    const [campaigns, setCampaigns] = useState([]);
+
+    function runCampaign() {
+
+    }
+
     return (
         <div>
-            <p>hi</p>
-            {campaign["campaign_type"]}
-            
+            {/* {campaign["campaign_type"]} */}
+            {/* Run Simulation Button */}
+            <button
+                onClick={() => runCampaign()}
+                className="inline-flex items-center px-4 py-2 text-black rounded focus:outline-none mb-4"
+            >
+                <svg
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+                </svg>
+                <label className="mb-1 font-semibold text-black">Run Simulation</label>
+            </button>
         </div>
     )
 }
